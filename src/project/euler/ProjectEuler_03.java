411f18c4fb3a24312e5d24ba2870c04ab852a42f
robertenglish <robert.english@live.ie>
2015-07-26 22:47:08 +0100

    Final submission for project 1, distributed systems.

---
+++ src/project/euler/ProjectEuler_03.java
@@ -0,0 +1,28 @@
+/*
+ * The prime factors of 13195 are 5,7,13 and 29.
+ * What is the largest prime factor of the number 600851475143?
+ */
+package project.euler;
+
+/**
+ *
+ * @author Robert
+ */
+public class ProjectEuler_03 {
+    
+    
+    public static void main (String[]args){
+    
+        double target = 13195;
+        double div = 3;
+        
+        do{
+        double res = target/div;
+        if (res % 2 >= 0){
+        }
+            
+        }while (target > 1);
+        
+        
+    }
+}
